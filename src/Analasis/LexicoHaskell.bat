de7d48627a0319e28b4f4811c888ea8fc9a0e342
titus1993 <titus.pivaral@gmail.com>
2017-03-02 16:41:57 -0600

    Creacion de archivos para los analizadores

--- src/Analasis/LexicoHaskell.bat
+++ src/Analasis/LexicoHaskell.bat
@@ -1,6 +1,6 @@
 SET JAVA_HOME = "C:\Program Files\Java\jre1.8.0_73\bin"
 SET PATH=%JAVA_HOME%;%PATH%
 SET CLASSPATH=%JAVA_HOME%;
-cd C:\Users\MishaPks\Documents\NetBeansProjects\[Compi2]Proyecto1_201020210\src\Analizadores
-java -jar C:\Users\MishaPks\Documents\NetBeansProjects\[Compi2]Proyecto1_201020210\lib\jflex-1.6.1\lib\jflex-1.6.1.jar Haskell_Lexico.jflex
+cd C:\Users\Titus\Documents\NetBeansProjects\[Compi2]Proyecto1_201213587\src\Analasis
+java -jar C:\Users\Titus\Documents\NetBeansProjects\[Compi2]Proyecto1_201213587\src\Librerias\jflex-1.6.0.jar Haskell_Lexico.jflex
 pause
--- src/Analasis/SintacticoHaskell.bat
+++ src/Analasis/SintacticoHaskell.bat
@@ -1,6 +1,6 @@
 SET JAVA_HOME = "C:\Program Files\Java\jre1.8.0_73\bin"
 SET PATH=%JAVA_HOME%;%PATH%
 SET CLASSPATH=%JAVA_HOME%;
-cd C:\Users\MishaPks\Documents\NetBeansProjects\[Compi2]Proyecto1_201020210\src\Analizadores
-java -jar C:\Users\MishaPks\Documents\NetBeansProjects\[Compi2]Proyecto1_201020210\lib\java-cup-11b.jar -parser SintacticoHaskell -symbols SimbolosHaskell SintacticoHaskell.cup
+cd C:\Users\Titus\Documents\NetBeansProjects\[Compi2]Proyecto1_201213587\src\Analasis
+java -jar C:\Users\Titus\Documents\NetBeansProjects\[Compi2]Proyecto1_201213587\src\Librerias\java-cup-11a.jar -parser Haskell_Sintactico -symbols sym Haskell_Sintactico.cup
 pause
