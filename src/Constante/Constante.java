e7c0345f6b3dc8bd51cc40dc68f4dda427382025
titus1993 <titus.pivaral@gmail.com>
2017-03-05 08:17:13 -0600

    Constantes terminado

--- src/Constante/Constante.java
+++ src/Constante/Constante.java
@@ -18,11 +18,11 @@ public class Constante {
     public static final String TCadena = "cadena";
     public static final String TCaracter = "caracter";
     public static final String TBool = "bool";
-    public static final String TObjeto = "objeto";
+    public static final String TAls = "als";
     public static final String TVacio = "vacio";
     
-    //puntuacion
-    public static final String TInterrogacion_Abre = "?";
+    //Simbolos y signos de puntuacion
+    public static final String TInterrogacion_Cierra = "?";
     public static final String TParentesis_Abre = "(";
     public static final String TParentesis_Cierra = ")";
     public static final String TLlave_Abre = "{";
@@ -30,6 +30,8 @@ public class Constante {
     public static final String TCorchete_Abre = "[";
     public static final String TCorchete_Cierra = "]";
     public static final String TPunto = ".";
+    public static final String TDolar = "$";
+    public static final String TComa = ",";
     
     
     //Visibilidad
@@ -42,12 +44,86 @@ public class Constante {
     public static final String TMenos = "-";
     public static final String TPor = "*";
     public static final String TDivision = "/";
+    public static final String TModulo = "mod";
+    public static final String TPotenciaH = "pot";
+    public static final String TPotenciaG = "^";
+    public static final String TIgual = "=";
+    
+    //Operaciones realcionales
+    public static final String TMayor = ">";
+    public static final String TMenor = "<";
+    public static final String TMayorIgual = ">=";
+    public static final String TMenorIgual = "<=";
+    public static final String TIgualacion = "==";
+    public static final String TDiferenciacion = "!=";
+    
+    
+    //operaciones condicionales
+    public static final String TAnd = "&&";
+    public static final String TOr = "||";
+    public static final String TXor = "&|";
+    public static final String TNot = "!";
+    
+    //operaciones exclusivas
+    public static final String TAumento = "++";
+    public static final String TDecremento = "--";
+    public static final String TIndiceLista = "!!";
+    
+    //operaciones en lista
+    public static final String TSum = "sum";
+    public static final String TProduct = "product";
+    public static final String TRevers = "revers";
+    public static final String TImpr = "impr";
+    public static final String TPar = "par";
+    public static final String TAsc = "asc";
+    public static final String TDesc = "desc";
+    public static final String TLength = "length";
+    
     
     //palabras reservadas
+    public static final String TLet = "let";
+    public static final String TEnd = "end";
+    public static final String TIf = "if";
+    public static final String TThen = "then";
+    public static final String TCase = "case";
+    
+    public static final String TCalcular = "calcular";
+    public static final String TSucc = "succ";
+    public static final String TDecc = "decc";
+    public static final String TMin = "min";
+    public static final String TMax = "max";
+    
     public static final String TVar = "var";
     public static final String TImportar = "importar";
-    public static final String TAls = "als";
+    public static final String THereda = "hereda";
+    public static final String TNuevo = "nuevo";
+    public static final String TRetornar = "retornar";
+    public static final String TLlamar = "llamar";
+    public static final String TInicio = "inicio";
+    public static final String TIncluirHK = "incluir_hk";
+    public static final String TLlamarHK = "llamarhk";
     
+    public static final String TSi = "si";
+    public static final String TSiNo = "sino";
+    public static final String TSeleccion = "seleccion";
+    public static final String TCaso = "caso";
+    public static final String TDefecto = "defecto";
+    
+    public static final String TPara = "para";
+    public static final String TMientras = "mientras";
+    public static final String THacer = "hacer";
+    
+    public static final String TContinuar = "continuar";
+    public static final String TTerminar = "terminar";
+    public static final String TArreglo = "arreglo";
+    public static final String TGraficarFuncion = "graphikar_funcion";
+    public static final String TDatos = "datos";
+    public static final String TColumna = "columna";
+    public static final String TProcesar = "procesar";
+    public static final String TDonde = "donde";
+    public static final String TDondeCada = "dondecada";
+    public static final String TDondeTodo = "dondetodo";
+    public static final String TImprimir = "imprimir";
     
     
 }
